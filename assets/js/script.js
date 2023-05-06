0fc6b757cec4c9c5d4d24e6065f887c719f5457b
Alex <alex.rednaks2000@gmail.com>
2023-05-06 09:25:19 +0100

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
-/**
- * navbar variables
- */
+/* navbar */
 
 const navOpenBtn = document.querySelector("[data-menu-open-btn]");
 const navCloseBtn = document.querySelector("[data-menu-close-btn]");
@@ -24,10 +22,7 @@ for (let i = 0; i < navElemArr.length; i++) {
 }
 
 
-
-/**
- * header sticky
- */
+/* sticky */
 
 const header = document.querySelector("[data-header]");
 
@@ -39,9 +34,7 @@ window.addEventListener("scroll", function () {
 
 
 
-/**
- * go top
- */
+/* top */
 
 const goTopBtn = document.querySelector("[data-go-top]");
 
